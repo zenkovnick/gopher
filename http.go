708af62ccdf4c34509d2483b03cac89047ffff3e
Nickolas <zenkovnick@gmail.com>
2015-06-15 08:42:56 +0300

    Create, get and delete value by key, storing in memory

--- http.go
+++ http.go
@@ -3,20 +3,63 @@ package main
 import (
 	"github.com/julienschmidt/httprouter"
     "net/http"
-	"time"
+//	"time"
 	"encoding/json"
+//	"strconv"
+//	"log"
+//	"io/ioutil"
+//	"net/http/httputil"
+//	"fmt"
+	"os"
+	"encoding/gob"
 )
+
+//var log = logging.MustGetLogger("example")
+
+var storage map[string]string
+
+type KeyItem struct {
+	Key 		string `json:"key"`
+	Content 	string `json:"content"`
+}
 
 type Response struct {
   Status    int
   Content   string
 }
 
-func handler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	t := time.Now().Local()
-	//fmt.Fprintf(w, t.Format("20060102150405"))
-	response := Response{200, t.Format("20060102150405")}
-	js, err := json.Marshal(response)
+func addKey(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var rec KeyItem
+	err := json.NewDecoder(r.Body).Decode(&rec)
+	if err != nil || rec.Key == "" || rec.Content == "" {
+		w.WriteHeader(400)
+		return
+	}
+	storage[rec.Key] = rec.Content
+
+	js, err := json.Marshal(rec)
+	w.WriteHeader(201) 
+	if err != nil {
+	  http.Error(w, err.Error(), http.StatusInternalServerError)
+	  return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+
+}
+
+func getKey(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := ps.ByName("key")
+	rec := KeyItem{Key: key}
+	if content, ok := storage[key]; ok {
+		rec.Content = content
+	} else {
+		w.WriteHeader(404)
+		return
+	}	
+
+	js, err := json.Marshal(rec)
+	w.WriteHeader(200) 
 	if err != nil {
 	  http.Error(w, err.Error(), http.StatusInternalServerError)
 	  return
@@ -25,9 +68,44 @@ func handler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Write(js)
 }
 
-func main() {
-	router := httprouter.New()
-	router.GET("/ping", handler)	
+func deleteKey(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := ps.ByName("key")
+	if _, ok := storage[key]; ok {
+		delete(storage, key)
+	} else {
+		w.WriteHeader(404)
+		return
+	}
+	w.WriteHeader(200) 
+}	
 	
+
+func persistToFile(path string, object interface{}) error {
+	file, err := os.Create(path)
+	if err == nil {
+		encoder := gob.NewEncoder(file)
+		encoder.Encode(object)
+	}
+	file.Close()
+	return err
+}
+
+func extractFromFile(path string, object interface{}) error {
+	file, err := os.Open(path)
+	if err == nil {
+		decoder := gob.NewDecoder(file)
+		err = decoder.Decode(object)
+	}
+	file.Close()
+	return err
+}
+
+
+func main() {
+	storage = make(map[string]string)
+	router := httprouter.New()
+	router.GET("/keys/:key", getKey)
+	router.POST("/keys", addKey)
+	router.DELETE("/keys/:key", deleteKey)
     http.ListenAndServe(":8080", router)
 }
